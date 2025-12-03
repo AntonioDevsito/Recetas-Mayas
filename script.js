f7f4a696bf4c75c25a29ccc62f2949b9d46dc95f
Antonio.Dev <90572637+AntonioDevsito@users.noreply.github.com>
2025-12-03 16:54:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const plantasData = [
         nombre: "Albahaca", nombreMaya: "Kakaltuum", equipo: "Equipo 6", tipo: "respiratorio", 
         cientifico: "Ocimum basilicum",
         desc: "Té para aliviar sofocación o estrés. Se recomienda una taza en ayunas y otra antes de dormir. (Precaución: No durante el embarazo).",
-        imagen: "https://pxhere.com/es/photo/75211"
+        imagen: "https://c.pxhere.com/photos/cd/82/photo-75211.jpg"
     },
     { 
         nombre: "Caña india", nombreMaya: "sak'ab o jíim", equipo: "Equipo N/A", tipo: "dolor", 
